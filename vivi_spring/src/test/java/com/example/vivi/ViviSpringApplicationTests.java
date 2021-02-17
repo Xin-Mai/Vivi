25a43f3fefd1b8cfb9bf7f0cf67fee10c831a30b
xin <857091490@qq.com>
2021-02-17 16:00:50 +0800

    udate

---
+++ vivi_spring/src/test/java/com/example/vivi/ViviSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViviSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
